0b74afc1e97a3eea51047d174cc97e2000a803ce
WAGDY 8.1 <WAGDY 8.1@Mallah>
2019-01-22 16:19:49 +0200

    ddd

---
+++ FINGER_PRINT_MAIN.c
@@ -0,0 +1,179 @@
+/*
+ * FINGER_PRINT_MAIN.c
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: WAGDY 8.1
+ */
+#include"FINGER_PRINT_MAIN.h"
+
+u8 FingerPrint_Store_Main(u16 PageId)
+{
+	//Initialize of Ack with failure befor recieving any package
+	u8 Ack=FailToFindMatchingFinger;
+	enum Ack_Packet Ack_Packet;
+	//Getting Image......
+	FingerPrint_CollectImage();
+	_delay_ms(300);
+	Ack=FingerPrint_AckPacket_All();
+	switch(Ack)
+	{
+	case(Command_Complete):
+	  Ack_Packet=Success;
+	break;
+	case(Command_ErrorRecPack):
+      return ErrorRecPack;
+	break;
+	case(Command_NoFingerOnSensor):
+	  return NoFingerOnSensor;
+	break;
+	case(Command_FailToDetectFinger):
+	  return FailToDetectFinger;
+	break;
+	}
+	//**********************
+	//Generate a character file and store it in char file one
+	FingerPrint_GenChar(Char_1);
+	_delay_ms(300);
+	Ack=FingerPrint_AckPacket_All();
+	switch(Ack)
+	{
+	case(Command_Complete):
+	  Ack_Packet=Success;
+	break;
+	case(Command_ErrorRecPack):
+	  return ErrorRecPack;
+	break;
+	case(Command_FailToGenCharFileOver ):
+	  return FailToGenCharFileOver;
+	break;
+	case(Command_FailToGenCharFileLackness):
+	  return FailToGenCharFileLackness;
+	break;
+	}
+	//**********************
+	//Generate a character file and store it in char file two
+	FingerPrint_GenChar(Char_2);
+	_delay_ms(300);
+	Ack=FingerPrint_AckPacket_All();
+	switch(Ack)
+	{
+	case(Command_Complete):
+	  Ack_Packet=Success;
+	break;
+	case(Command_ErrorRecPack):
+	  return ErrorRecPack;
+	break;
+	case(Command_FailToGenCharFileOver ):
+	  return FailToGenCharFileOver;
+	break;
+	case(Command_FailToGenCharFileLackness):
+	  return FailToGenCharFileLackness;
+	break;
+	}
+	//*********************
+	//Generate a template combination from charcter file one and two
+	 FingerPrint_GenTemplate();
+	 _delay_ms(300);
+	Ack=FingerPrint_AckPacket_All();
+	switch(Ack)
+	{
+	case(Command_Complete):
+	  Ack_Packet=Success;
+	break;
+	case(Command_ErrorRecPack):
+	  return ErrorRecPack;
+	break;
+	case(Command_FailToCombCharFile ):
+	  return FailToCombCharFile;
+	break;
+	}
+	//*******************
+	//Store this template in the page Id
+	 FingerPrint_Store(Char_1,PageId);
+	 _delay_ms(300);
+	Ack=FingerPrint_AckPacket_All();
+	switch(Ack)
+	{
+	case(Command_Complete):
+	  Ack_Packet=Success;
+	break;
+	case(Command_ErrorRecPack):
+	  return ErrorRecPack;
+	break;
+	case(Command_ErrorAddrPageID ):
+	  return ErrorAddrPageID;
+	break;
+	case(Command_ErrorWrittingFlash ):
+	  return ErrorWrittingFlash;
+	break;
+	}
+	//*********************
+	return Ack_Packet;
+}
+u8 FingerPrint_Search_Main()
+{
+	//Initialize of Ack with failure befor recieving any package
+	u8 Ack=FailToFindMatchingFinger;
+	enum Ack_Packet Ack_Packet;
+	//Getting Image......
+	FingerPrint_CollectImage();
+	_delay_ms(300);
+	Ack=FingerPrint_AckPacket_All();
+	switch(Ack)
+	{
+	case(Command_Complete):
+	  Ack_Packet=Success;
+	break;
+	case(Command_ErrorRecPack):
+      return ErrorRecPack;
+	break;
+	case(Command_NoFingerOnSensor):
+	  return NoFingerOnSensor;
+	break;
+	case(Command_FailToDetectFinger):
+	  return FailToDetectFinger;
+	break;
+	}
+	//**********************
+	//Generate a character file and store it in char file one
+	FingerPrint_GenChar(Char_1);
+	_delay_ms(300);
+	Ack=FingerPrint_AckPacket_All();
+	switch(Ack)
+	{
+	case(Command_Complete):
+	  Ack_Packet=Success;
+	break;
+	case(Command_ErrorRecPack):
+	  return ErrorRecPack;
+	break;
+	case(Command_FailToGenCharFileOver ):
+	  return FailToGenCharFileOver;
+	break;
+	case(Command_FailToGenCharFileLackness):
+	  return FailToGenCharFileLackness;
+	break;
+	}
+	//**********************
+	//Searching for the char file one
+	 FingerPrint_Search();
+	 _delay_ms(300);
+	Ack=FingerPrint_AckPacket_All();
+	switch(Ack)
+	{
+	case(Command_Complete):
+	  Ack_Packet=Success;
+	break;
+	case(Command_ErrorRecPack):
+	  return ErrorRecPack;
+	break;
+	case(Command_ErrorAddrPageID ):
+	  return ErrorAddrPageID;
+	break;
+	case(Command_ErrorWhenRecTemplate):
+	  return ErrorWhenRecTemplate;
+	break;
+	}
+	//*********************
+	return Ack_Packet;
+}
---
+++ FINGER_PRINT_MAIN.h
@@ -0,0 +1,42 @@
+/*
+ * FINGER_PRINT_MAIN.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: WAGDY 8.1
+ */
+
+#ifndef FINGER_PRINT_MAIN_H_
+#define FINGER_PRINT_MAIN_H_
+
+#include"FINGER_PRINT_MODULE.h"
+
+//***********
+#define Char_1   0x01
+#define Char_2   0x02
+
+enum Ack_Packet
+{
+	Success,
+	NoFingerOnSensor,
+	FailToDetectFinger,
+    FingerNotMatch,
+	FailToFindMatchingFinger,
+	FailToCombCharFile,
+	ErrorRecPack,
+	FailToGenCharFileLackness,
+	FailToGenCharFileOver,
+	ErrorAddrPageID,
+	ErrorWrittingFlash,
+	ErrorWhenRecTemplate,
+};
+//***********
+
+//detect the finger and store it in the page id specified
+u8 FingerPrint_Store_Main(u16 PageId);
+//*******************
+
+//detect the finger and search for it.return the page id of the finger detected
+u8 FingerPrint_Search_Main(void);
+//******************
+
+#endif /* FINGER_PRINT_MAIN_H_ */
