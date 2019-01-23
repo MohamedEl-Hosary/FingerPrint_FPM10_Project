c18dd7882ef380e8a4190039c036f4673315665d
WAGDY 8.1 <WAGDY 8.1@Mallah>
2019-01-23 12:11:00 +0200

    commit

--- UART-CONFIG.h
+++ UART-CONFIG.h
@@ -1,7 +1,7 @@
 #ifndef UART_H_config
 #define UART_H_config
 #include <avr/io.h>
-#define  Baudrate    1200
+#define  Baudrate    57600
 #define F_CPU 8000000UL
 #define  UBRRVALUE   ((F_CPU)/(Baudrate*16UL)-1UL)
 
--- main.c
+++ main.c
@@ -4,5 +4,14 @@
  *  Created on: 21 Jan 2019
  *      Author: Mohamed El-Hosary
  */
+#include "FINGER_PRINT_MAIN.h"
 
+void main(void)
+{
+	u8 Ack=FingerPrint_Store_Main(0x01);
+	switch(Ack)
+	{
+	case Success:
 
+	}
+}
